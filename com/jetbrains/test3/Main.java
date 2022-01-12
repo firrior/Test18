14e22852a2eaded7118f90a4728a519b32efdd9f
Nadia <firrior@gmail.com>
2022-01-12 07:36:13 -0800

    Commit 2

--- com/jetbrains/test3/Main.java
+++ com/jetbrains/test3/Main.java
@@ -4,4 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int i = 0;
     }
+
+    public static void ExtraMethod(){
+        // do something
+        int i = 0;
+        int k = i;
+    }
 }
